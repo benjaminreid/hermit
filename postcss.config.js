02cc9757c6f81977ed1333824a7d29c9a414af26
Benjamin Reid <benjaminreid.nouveller@gmail.com>
2020-10-13 20:09:00 +0100

    Compress CSS in production with CSSNano

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = {
-  plugins: [require("tailwindcss")],
+  plugins: [
+    require("tailwindcss"),
+    ...(isProduction
+      ? [
+          require("cssnano")({
+            preset: "default",
+          }),
+        ]
+      : []),
+  ],
 };
