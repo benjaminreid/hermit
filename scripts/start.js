27970441be2da0c5e836a3f93c7a824166a97283
Benjamin Reid <benjaminreid.nouveller@gmail.com>
2020-10-13 19:30:32 +0100

    Copy index file when changes are made

--- scripts/start.js
+++ scripts/start.js
@@ -1,7 +1,16 @@
 const bs = require("browser-sync").create();
+const fs = require("fs-extra");
+
+bs.watch("src/index.html", async (event) => {
+  if (event === "change") {
+    const index = await fs.readFile("src/index.html");
+    await fs.outputFile("dist/index.html", index);
+  }
+});
 
 bs.watch("./dist/index.html").on("change", bs.reload);
 
 bs.init({
-  server: "./dist",
+  server: "dist",
+  open: false,
 });
